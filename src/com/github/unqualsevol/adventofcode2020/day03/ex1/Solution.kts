096954173dae6e6c1f22f5fff34e4d29c7f0abf7
Aleix Gimenez <11557635+unQualsevol@users.noreply.github.com>
2020-12-03 15:27:27 +0100

    solve third day

---
+++ src/com/github/unqualsevol/adventofcode2020/day03/ex1/Solution.kts
@@ -0,0 +1,14 @@
+import java.io.File
+
+val increment = 3
+var current = 0
+val tree = '#'
+val openSquare = '.'
+
+var treesCount = 0;
+File("../input").forEachLine {
+    val linePattern = it
+    if(linePattern.get(current) == tree) treesCount++;
+    current = (current + increment) % linePattern.length
+}
+println("trees: $treesCount")

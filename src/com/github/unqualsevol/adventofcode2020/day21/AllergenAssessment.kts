b3dbe59b7c972d317ed84e4a4a5896385b632da7
Aleix Gimenez <11557635+unQualsevol@users.noreply.github.com>
2020-12-21 20:01:25 +0100

    solve twenty-oneth day

---
+++ src/com/github/unqualsevol/adventofcode2020/day21/AllergenAssessment.kts
@@ -0,0 +1,43 @@
+import java.io.File
+
+val foods = File("input").readLines().map {
+    val ingredients = it.substringBefore('(').trim().split(" ")
+    val allergens = it.substring(it.indexOf('(') + 1, it.lastIndex).replace("contains ", "").split(", ")
+    Pair(ingredients, allergens)
+}
+
+val ingredients = foods.map { it.first }.flatten().toSet()
+val allergens = foods.map { it.second }.flatten().toSet()
+
+val ingredientToFood = ingredients.map { ingredient ->
+    ingredient to
+            foods.filter { it.first.contains(ingredient) }
+}.toMap()
+
+var count = 0
+val allergenToFood = allergens.map { allergen ->
+    allergen to foods.filter { food -> food.second.contains(allergen) }.map { food -> food.first.toMutableList() }
+}.toMap()
+
+val mapAllergenIngredient = mutableMapOf<String, MutableList<String>>()
+var currentIngredients = ingredients.toMutableList()
+for (allergen in allergenToFood) {
+    for (ingredient in currentIngredients) {
+        if (allergen.value.all { it.contains(ingredient) }) {
+            mapAllergenIngredient.getOrPut(allergen.key, { mutableListOf() }).add(ingredient)
+        }
+    }
+}
+
+while (!mapAllergenIngredient.all { it.value.size == 1 }) {
+    val uniqueAllergenIngredient = mapAllergenIngredient.filter { it.value.size == 1 }.map { it.value }.flatten()
+    mapAllergenIngredient.filter {it.value.size > 1}.forEach {
+        it.value.removeAll(uniqueAllergenIngredient)
+    }
+}
+mapAllergenIngredient.forEach { println(it) }
+
+val result = foods.map { it.first }.flatten().filterNot { mapAllergenIngredient.values.flatten().contains(it) }.count()
+println("times do any of those ingredients appear $result")
+
+println("canonical dangerous ingredient list: ${mapAllergenIngredient.toSortedMap().map { it.value }.flatten().joinToString (",")}")
